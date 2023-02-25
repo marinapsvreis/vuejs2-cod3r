ab48a4fc86c70def47ef2407676e4ede4c659c6b
Marina Portugal <marinapsvreis@gmail.com>
2023-02-24 22:24:20 -0300

    projeto simples com vue cli

---
+++ vue-cli/projeto-simples/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    {{ titulo }}
+    <button @click="titulo += '#'">Add #</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      titulo: "Testando o título"
+    }
+  },
+}
+</script>
+
+<style>
+
+#app {
+  background-color: chocolate;
+  color: #fff;
+}
+
+</style>
