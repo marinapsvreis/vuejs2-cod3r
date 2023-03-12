59de7fa8e80b48d2882d33003cf22cb9fef82610
Marina Portugal <marinapsvreis@gmail.com>
2023-03-12 18:38:52 -0300

    Desafio concluido

---
+++ componentes/componente-desafio/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <menu-lateral></menu-lateral>
+
+    <toolbar></toolbar>
+
+    <carrosel></carrosel>
+
+    <barra-inferior></barra-inferior>
+  </v-app>
+</template>
+
+<script>
+import Carrossel from './components/Carrossel.vue';
+import MenuLateral from './components/MenuLateral.vue';
+import Toolbar from './components/Toolbar.vue';
+import BarraInferior from './components/BarraInferior.vue';
+
+export default {
+  data() {
+    return {}
+  },
+  components: {
+    'menu-lateral': MenuLateral,
+    'carrosel': Carrossel,
+    'toolbar': Toolbar,
+    'barra-inferior': BarraInferior,
+  }
+}
+</script>
