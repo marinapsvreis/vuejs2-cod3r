fd3d8543287b5fe886b77a9c597afddca5b3c340
Marina Portugal <marinapsvreis@gmail.com>
2023-03-11 21:23:44 -0300

    primeiro modulo de componentes ainda faltando resolver o desafio

---
+++ componentes/componente-intro/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h1>Contadores</h1>
+    <app-contadores v-for="c in 5" :key="c"/>
+    <button>?</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 0
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ componentes/componente-intro/src/Contadores.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="contadores">
+    <app-contador></app-contador>
+  </div>
+</template>
+
+<script>
+import Contador from './Contador.vue'
+export default {
+    components: {
+        'app-contador': Contador
+    }
+}
+</script>
+
+<style>
+
+</style>
